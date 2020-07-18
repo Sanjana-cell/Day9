6e78ed5f35d45b8ee98ce887a4367a3a5c6036eb
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 09:22:11 +0530

    Refactor: modified code by adding switch case instead of condition statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,13 @@
 
 WAGE_PER_HOUR=20
 workHours=0
-
+IS_PART_TIME=1
+IS_FULL_TIME=2
 attendence=$(($RANDOM%2+1))
-if [ $attendence -eq 2 ]
-then
-	 workHours=8
-else
-	 workHours=4
-fi
+case $attendence in
+        $IS_PART_TIME)  workHours=4;;
+        $IS_FULL_TIME)  workHours=8;;
+        *)              workHours=0;;
+esac
 dailyWage=$(($workHours*$WAGE_PER_HOUR))
+echo "DailyWage " $dailyWage
