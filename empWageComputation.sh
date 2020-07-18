e43a20515d9387ffc896e4115ce218fe8c2be06d
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 09:31:23 +0530

    Refactor: modified code to calculate dailyWages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,25 @@
 #!/bin/bash -x
 
 WAGE_PER_HOUR=20
-workHours=0
 IS_PART_TIME=1
 IS_FULL_TIME=2
-attendence=$(($RANDOM%2+1))
-case $attendence in
-        $IS_PART_TIME)  workHours=4;;
-        $IS_FULL_TIME)  workHours=8;;
-        *)              workHours=0;;
-esac
-dailyWage=$(($workHours*$WAGE_PER_HOUR))
-echo "DailyWage " $dailyWage
+MAX_WORKING_DAYS=20
+
+workhours=0
+attendence=0
+counter=0
+totalWages=0
+while [ $counter -lt $MAX_WORKING_DAYS ]
+do
+	attendence=$(($RANDOM%2+1))
+	case $attendence in
+        	$IS_PART_TIME)  workHours=4;;
+        	$IS_FULL_TIME)  workHours=8;;
+        	*)              workHours=0;;
+	esac
+	dailyWage=$(($workHours*$WAGE_PER_HOUR))
+	totalWages=$(($totalWages+$dailyWage))
+	echo "DailyWage " $dailyWage
+	((counter++))
+done
+echo "Total Wages " $totalWages
