acaac2a5881983f43fb33f1670afa32b7b6a609f
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 09:42:50 +0530

    Refactor: modified code to calculate wages till a condition of total working hours is reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -x
 
+#constants
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
 MAX_WORKING_DAYS=20
+MAX_WORKING_HRS=100
 
+#variables
 workhours=0
 attendence=0
 counter=0
+totalWorkingDays=1
+totalWorkingHrs=0
 totalWages=0
-while [ $counter -lt $MAX_WORKING_DAYS ]
+
+while [  $totalWorkingDays -lt $MAX_WORKING_DAYS ] && [  $totalWorkingHrs -lt $MAX_WORKING_HRS ]
 do
 	attendence=$(($RANDOM%2+1))
 	case $attendence in
@@ -19,7 +25,8 @@ do
 	esac
 	dailyWage=$(($workHours*$WAGE_PER_HOUR))
 	totalWages=$(($totalWages+$dailyWage))
-	echo "DailyWage " $dailyWage
-	((counter++))
+	echo "DailyWage on $totalWorkingDays" $dailyWage
+	totalWorkingHrs=$(($totalWorkingHrs+$workHours))
+	((totalWorkingDays++))
 done
 echo "Total Wages " $totalWages
