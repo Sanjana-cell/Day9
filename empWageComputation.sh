a4ca9ad48a96c5fecec0a098b96af44857783ba8
Sanjana-cell <sanjanakr97@gmail.com>
2020-07-18 09:15:51 +0530

    Refactor: modified code calculate daily employee wage with part time work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
 WAGE_PER_HOUR=20
-IS_PART_TIME=1
-IS_FULL_TIME=2
 workHours=0
 
-attendence=$(($RANDOM%2))
-if [ $attendence -eq 1 ]
+attendence=$(($RANDOM%2+1))
+if [ $attendence -eq 2 ]
 then
 	 workHours=8
+else
+	 workHours=4
 fi
 dailyWage=$(($workHours*$WAGE_PER_HOUR))
